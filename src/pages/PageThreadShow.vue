e50126193a953cecec9b51361bb5397d83d2097f
Manuel Sousa <mlrcbsousa@gmail.com>
2019-07-17 08:13:46 +0200

    user profile and font awesome

--- src/pages/PageThreadShow.vue
+++ src/pages/PageThreadShow.vue
@@ -24,12 +24,10 @@ export default {
   props: {
     id: { type: String, required: true }
   },
-  data () {
-    return {
-      thread: this.$store.state.threads[this.id]
-    };
-  },
   computed: {
+    thread () {
+      return this.$store.state.threads[this.id]
+    },
     posts () {
       const postIds = Object.values(this.thread.posts)
       return Object.values(this.$store.state.posts)
